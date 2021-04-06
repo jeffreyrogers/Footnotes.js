b29ed1ed9a3b36240d9a561f805df896fd55865f
Jeff Rogers <jeffreyrogers27@gmail.com>
2021-04-06 17:26:54 -0600

    Add basic sidenote implementation
    Sidenotes now display properly, however they are not properly positioned
    yet. I also made the classlists that get added to the dynamically
    created elements constants so that they can be more easily customized.

--- footnotes.ts
+++ footnotes.ts
@@ -1,4 +1,8 @@
+// These constants can be customized to whatever values are appropriate for your application
 const fnContainerName = "footnote-container";
+const footnoteClasses = ['rounded', 'bg-gray-900', 'px-2', 'py-1', 'my-1', 'shadow-well'];
+const linkClasses = ['link'];
+const sidenoteClasses = ['mb-2'];
 
 // Haters will say you shouldn't use globals, but this is the clearest way to handle this in my opinion.
 declare global {
@@ -25,9 +29,13 @@ function updateFootnotes() {
     let sidenote: boolean = false;
     let href: string;
 
+    // Since the sidenote container will only be visible if the page is wide enough to display
+    // sidenotes, we simply check for its visibility to determine whether to handle references as
+    // footnotes or sidenotes.
     let fnContainer = document.getElementById(fnContainerName);
     if (fnContainer && window.getComputedStyle(fnContainer, null).display === 'block') {
         sidenote = true;
+        fnContainer.textContent = ""; // clear content (prevents re-adding the same footnotes)
         href = "#sidenote";
     } else {
         href = "#footnote";
@@ -38,7 +46,7 @@ function updateFootnotes() {
         counter += 1;
 
         fn.style.display = 'none';
-        fn.classList.add('rounded', 'bg-gray-900', 'px-2', 'py-1', 'my-1', 'shadow-well');
+        fn.classList.add(...footnoteClasses);
 
         // remove the footnote number if it already exists so that we can update it after the resize
         let expired_fn = document.getElementById('reference' + String(counter));
@@ -50,23 +58,19 @@ function updateFootnotes() {
         reference.id = "reference" + String(counter);
         let link = document.createElement('a');
         link.innerHTML = String(counter);
-        link.classList.add('link');
+        link.classList.add(...linkClasses);
         link.setAttribute('href', href + String(counter));
         reference.appendChild(link);
         fn.insertAdjacentHTML('beforebegin', reference.outerHTML);
 
         if (sidenote) {
             // add fn.innerHTML to fnContainer
-            let _fn = fn;
-            let _ref = document.getElementById("reference" + String(counter));
+            console.log(fn.innerHTML);
 
-            _ref.addEventListener("click", function() {
-                if (_fn.style.display === "none") {
-                    _fn.style.display = "block";
-                } else {
-                    _fn.style.display = "none";
-                }
-            }, false);
+            let sn = document.createElement('div');
+            sn.classList.add(...sidenoteClasses);
+            sn.innerHTML = String(counter) + ". " + fn.innerHTML;
+            fnContainer.appendChild(sn);
         } else {
             let _fn = fn;
             let _ref = document.getElementById("reference" + String(counter));
